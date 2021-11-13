544647e3af2c779e44b69e488254e6b236494f24
MaiBaoNam2001 <maibaonam260219@gmail.com>
2021-11-13 12:18:41 +0700

    update

--- TestGitHubDemo/src/Main/Demo.java
+++ TestGitHubDemo/src/Main/Demo.java
@@ -1,12 +1,12 @@
 package Main;
 
 public class Demo {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		int a = 10, b = 5;
 		if (a == 11) {
 			System.out.println("Hello World !");
 		}
 		else System.out.println("Hello Java");
-		System.out.println("Hello PHP");
+		Thread.sleep((1000));
 	}
 }
