84cc0188f30d111b5242623031dd8d3c1a724882
MaiBaoNam2001 <maibaonam260219@gmail.com>
2021-11-13 11:25:10 +0700

    feature-test init

--- TestGitHubDemo/src/Main/Demo.java
+++ TestGitHubDemo/src/Main/Demo.java
@@ -2,7 +2,10 @@ package Main;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("Hello World !");
+		int a = 10, b = 5;
+		if (a == 10) {
+			System.out.println("Hello World !");
+		}
 		System.out.println("Hello Java");
 	}
 }
