fd721a3682c4c1be41d44182aab297c37b87f536
MaiBaoNam2001 <maibaonam260219@gmail.com>
2021-11-13 11:31:42 +0700

    fixed

--- TestGitHubDemo/src/Main/Demo.java
+++ TestGitHubDemo/src/Main/Demo.java
@@ -3,9 +3,9 @@ package Main;
 public class Demo {
 	public static void main(String[] args) {
 		int a = 10, b = 5;
-		if (a == 10) {
+		if (a == 5) {
 			System.out.println("Hello World !");
 		}
-		System.out.println("Hello Java");
+		else System.out.println("Hello Java");
 	}
 }
